1947b5ea075f6dbb483e88c65c7755fe6e9fcc64
renzoSES <renzoravines10@gmail.com>
2022-09-08 04:01:47 -0500

    Editado por Renzo

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-
+       //Editado por Renzo Ravines
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
         toyo.avanzar();
